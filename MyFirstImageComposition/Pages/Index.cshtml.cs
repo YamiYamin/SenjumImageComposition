cf45997c7473498f8afdd9a38321e9de0738d3bf
Yamin Satsuki <yamada.osaru@gmail.com>
2021-03-27 01:41:18 +0900

    名前・兵種・技種・成長回数が自動で合成されるようにした。

--- MyFirstImageComposition/Pages/Index.cshtml.cs
+++ MyFirstImageComposition/Pages/Index.cshtml.cs
@@ -28,22 +28,64 @@ namespace MyFirstImageComposition.Pages
 
         public void OnGet()
         {
-            string rootPath = _hostEnvironment.WebRootPath;
+            // 兵オブジェクトが持っている情報
+            int nameA = 1;
+            int nameB = 1;
+            string name = "京ノ介";
+            int ch = 1;
+            int ac = 1;
+            string ss = "s1s2s3s4s5s7s8s9s10s11s12s13s14s15s16s17s18s19s20s21s22s33s32s90s";
+            int mp = 0;
+            int kp = 0;
+            int pw = 0;
+            int df = 0;
+            int s = 4;
 
-            string baseImagePath = Path.Combine(rootPath, @"images\no_title.png");
 
-            using Bitmap newImage = new(baseImagePath);
-            using Graphics g = Graphics.FromImage(newImage);
+            // 元画像
+            string baseImagePath = GetAbsolutePath(@"images\no_title.png");
+            using Bitmap soldierImage = new(baseImagePath);
 
-            string ss = "s1s2s3s4s5s7s8s9s10s11s12s13s14s15s16s17s18s19s20s21s22s33s32s";
+            using (Graphics g = Graphics.FromImage(soldierImage))
+            {
+                // 兵名
+                g.DrawString(name, new Font("MS ゴシック", 11, FontStyle.Bold), Brushes.Moccasin, new Point(133, 21));
 
+                // 兵種
+                string chImagePath = GetAbsolutePath($@"images\characters\ch{ch}.png");
+                using Bitmap chImage = new(chImagePath);
+                g.DrawImage(chImage, 178, 45);
+
+                // 技種
+                string acImagePath = GetAbsolutePath($@"images\actions\ac{ac}.png");
+                using Bitmap acImage = new(acImagePath);
+                g.DrawImage(acImage, 297, 45);
+
+                // 作戦・特殊能力
+                DrawSpecialSkills(g, ss);
+            }
+
+            // 文字列に変換
+            TestImage = ImageToString(soldierImage);
+        }
+
+        private string GetAbsolutePath(string path)
+        {
+            return Path.Combine(_hostEnvironment.WebRootPath, path);
+        }
+
+        // 作戦・特殊能力・向き・作戦を合成
+        private void DrawSpecialSkills(Graphics g, string ss)
+        {
+            // 作戦・特殊能力
             for (int i = 1; i <= 34; i++)
             {
+                // 未割り当て
                 if (i is 6 or 23 or 24 or 30)
                 {
                     continue;
                 }
-
+                // 向き
                 if (i is >= 25 and <= 29)
                 {
                     continue;
@@ -51,13 +93,23 @@ namespace MyFirstImageComposition.Pages
 
                 if (ss.Contains($"s{i}s"))
                 {
-                    string ssImagePath = Path.Combine(rootPath, $@"images\skills\s{i}s.png");
-                    using Bitmap logoImage = new(ssImagePath);
-                    g.DrawImage(logoImage, CalcSSPoint(i));
+                    string ssImagePath = GetAbsolutePath($@"images\skills\s{i}s.png");
+                    using Bitmap ssImage = new(ssImagePath);
+                    g.DrawImage(ssImage, CalcSpecialSkillPoint(i));
                 }
             }
 
-            TestImage = ImageToString(newImage);
+            // 成長
+            for (int i = 90; i <= 99; i++)
+            {
+                if (ss.Contains($"s{i}s"))
+                {
+                    string ssImagePath = GetAbsolutePath($@"images\skills\s{i}s.png");
+                    using Bitmap ssImage = new(ssImagePath);
+                    g.DrawImage(ssImage, 289, 101);
+                    break;
+                }
+            }
         }
 
         private readonly Dictionary<int, (int, int)> Coordinates = new()
@@ -82,7 +134,7 @@ namespace MyFirstImageComposition.Pages
             { 22, (192, 269) },
         };
 
-        public Point CalcSSPoint(int a)
+        public Point CalcSpecialSkillPoint(int a)
         {
 
             if (a is 6 or 23 or 24 or 30)
@@ -96,7 +148,7 @@ namespace MyFirstImageComposition.Pages
 
             int[] gaps = { 0, 5, 9, 14, 18, 23 };
 
-            int width = leftWidth;
+            int width = 0;
             int height = ssHeight;
 
             // 作戦行動
@@ -104,21 +156,21 @@ namespace MyFirstImageComposition.Pages
             {
                 int gap = gaps[a - 1];
 
-                width += ssWidth * (a - 1) + gap;
+                width = leftWidth + ssWidth * (a - 1) + gap;
             }
             // 特殊能力
             else if (a is > 6 and < 23)
             {
-                Coordinates.TryGetValue(a, out (int x, int y) z);
-                width = z.x;
-                height = z.y;
+                Coordinates.TryGetValue(a, out (int x, int y) coord);
+                width = coord.x;
+                height = coord.y;
             }
             // 特殊能力(赤)
             else if (a is 31 or 33 or 34)
             {
                 int gap = gaps[4];
 
-                width += ssWidth * 4 + gap;
+                width = leftWidth + ssWidth * 4 + gap;
                 height = 269;
             }
             else if (a is 32)
